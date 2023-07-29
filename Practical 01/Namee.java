6a57e283fa548bf43e4f21cd9336fedb357017c8
Awishka Dilshan Yapa <139097573+Awishkadilshanyapa@users.noreply.github.com>
2023-07-29 20:33:46 +0530

    Add files via upload

---
+++ Practical 01/Namee.java
@@ -0,0 +1,12 @@
+
+package namee;
+
+public class Namee {
+
+    public static void main(String[] args) {
+        System.out.println("Awishka Dilshan Yapa");
+        System.out.println("Computer Science");
+
+    }
+    
+}
