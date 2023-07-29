93a7f30041e45573fc27896a5f4cc9c49931f241
Awishka Dilshan Yapa <139097573+Awishkadilshanyapa@users.noreply.github.com>
2023-07-29 20:24:59 +0530

    Add files via upload

---
+++ Practical 01/Loop.java
@@ -0,0 +1,21 @@
+
+package loop;
+
+public class Loop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Executing Loop " + i);
+        }
+        
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Executing Loop " + i);
+            i++;
+        }
+
+    
+    }
+}
+   
+
